45a13e5387ce5da2ff652a2f57af20d0cdac7a71
OleksiiChiiachenko <129313262+OleksiiChiiachenko@users.noreply.github.com>
2026-03-27 18:33:20 +0000

    11-1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export function getImagesByQuery(query) {
+  const BASE_URL = 'https://pixabay.com/api/';
+
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: '55050395-20e5244c38068275f8aacdcf3',
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data.hits);
+}
